9f0b2ed9b3ae9f6d47bb2b37c053aca67cf2aeb1
Chaaru <chaaru@beyondgames.co>
2015-08-13 15:40:50 -0700

    Server triggers event on each pullLever

--- game/Client.fs
+++ game/Client.fs
@@ -2,31 +2,33 @@
 
 open Accounts.Account
 
-let startGame (money :int) (buyIn :int) :Account = 
-    Server.initialize money buyIn
+let startGame (money :int) (buyIn :int) = 
+    let server = new Server.Server ()
+    let acct = server.Initialize money buyIn
+    acct, server
 
-let pullLever rng account :Account = 
-    Server.doLeverPull account rng
+let pullLever rng (server:Server.Server) account :Account = 
+    server.DoLeverPull account rng
 
 let gameOver () = 
     printfn "Player has decided to stop playing"
     emptyAccount
 
-let rec gameLoop (i :int) (rng : System.Random) (account :Account) :Account =
+let rec gameLoop (i :int) (rng : System.Random) (server : Server.Server) (account :Account) :Account =
     printfn "gameLoop iteration %A and %A" i account
     match i >= 300 with
     | true -> gameOver ()
     | false -> 
         match leverPullable account with
         | true -> 
-            pullLever rng account
-            |> gameLoop (i+1) rng
+            pullLever rng server account
+            |> gameLoop (i+1) rng server
         | _ -> gameOver ()
 
 [<EntryPoint>]
 let main argv = 
-    let initialAccount = startGame 10000 10
+    let initialAccount, server = startGame 10000 10
     let rng = new System.Random 4
-    let finalAccount = gameLoop 0 rng initialAccount
+    let finalAccount = gameLoop 0 rng server initialAccount
     0 // return an integer exit code
 
--- game/Server.fs
+++ game/Server.fs
@@ -2,33 +2,40 @@
 
 open Accounts.Account
 
-let initialize money buyIn = 
-    match money > buyIn with
-    | true -> Account [Money(money); BuyIn(buyIn)]
-    | false -> 
-        printfn "Your account does not have enough money to start a new session"
-        emptyAccount
+type Server () = 
 
-let doLeverPull account (random :System.Random) =
-//    let random = System.Random ()
-    let r1 = random.Next(1,10)
-    let r2 = random.Next(1,10)
-    let buyIn = getBuyIn account
-    let currentMoney = getMoney account
-    match leverPullable account with
-    | true ->
-        match buyIn, currentMoney with
-        | Some buyIn, Some currentMoney ->  
-            let payout = buyIn * 10
-            match r1 = r2 with
-            | true -> 
-                printfn "You won"
-                Account [Money(payout+currentMoney); BuyIn(buyIn)]
-            | false ->
-                Account [Money(currentMoney-buyIn); BuyIn(buyIn)]
-        | _, _ -> 
-            printfn "Your account does not have money or buyIn information"
+    let serverEvent = new Event<Account> ()
+
+    member self.Events () = serverEvent.Publish
+
+    member self.Initialize money buyIn = 
+        match money > buyIn with
+        | true -> Account [Money(money); BuyIn(buyIn)]
+        | false -> 
+            printfn "Your account does not have enough money to start a new session"
             emptyAccount
-    | false -> 
-        printfn "Your account does not have enough money to pull the lever"
-        emptyAccount
+
+    member self.DoLeverPull account (random :System.Random) =
+        let r1 = random.Next(1,10)
+        let r2 = random.Next(1,10)
+        let buyIn = getBuyIn account
+        let currentMoney = getMoney account
+        match leverPullable account with
+        | true ->
+            match buyIn, currentMoney with
+            | Some buyIn, Some currentMoney ->  
+                let payout = buyIn * 10
+                serverEvent.Trigger (account)
+                match r1 = r2 with
+                | true -> 
+                    printfn "You won"
+                    Account [Money(payout+currentMoney); BuyIn(buyIn)]
+                | false ->
+                    Account [Money(currentMoney-buyIn); BuyIn(buyIn)]
+            | _, _ -> 
+                printfn "Your account does not have money or buyIn information"
+                emptyAccount
+        | false -> 
+            printfn "Your account does not have enough money to pull the lever"
+            emptyAccount
+
