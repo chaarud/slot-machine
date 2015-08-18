97b63597ca168084978a035b0f1c35e9190b64e9
Chaaru <chaaru@beyondgames.co>
2015-08-17 17:14:45 -0700

    Amplitude's HTTP API successfully called.

--- game/Client.fs
+++ game/Client.fs
@@ -1,6 +1,18 @@
 ﻿module Client
 
 open Accounts.Account
+//open System.Net
+//open System.Text
+open FSharp.Data
+
+
+
+let sendRequest () = //id event = 
+    let url = "https://api.amplitude.com/httpapi"
+    let api_key = "9d7c111da3e4eaa3e6c2a36f90283424"
+    let event = """[{"user_id":"Bob","event_type":"loginEvent"}]"""
+    let requestBody = FormValues[("api_key", api_key ); ("event", event)]
+    Http.RequestString(url, body = requestBody)
 
 let startGame money buyIn = 
     let server = new Server.Server ()
@@ -27,12 +39,16 @@ let rec gameLoop i rng server account =
 
 [<EntryPoint>]
 let main argv = 
-    let initialAccount, server = startGame 10000 10
-    let rng = new System.Random ()
+//    let initialAccount, server = startGame 10000 10
+//    let rng = new System.Random ()
 
-    let playerId = rng.Next (1, 100000000)
-    let playerDevice = "iPhone"
+    let x = sendRequest ()
+    printfn "%A" x
 
-    let finalAccount = gameLoop 0 rng server initialAccount
+//    let idNum = rng.Next (1, 100000000) 
+//    let id = idNum.ToString ()
+//    let playerDevice = "iPhone"
+
+//    let finalAccount = gameLoop 0 rng server initialAccount
     0 // return an integer exit code
 
