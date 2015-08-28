3f61093f11488f40d457ff16ecfb3f524e998d9b
Chaaru <chaaru@beyondgames.co>
2015-08-27 17:50:40 -0700

    Changed paket to use websocket-sharp-BeyondGames. Added references for Zlib.Portable dll to Slots.iOS and SlotMachine.

--- SlotMachine/Main.fs
+++ SlotMachine/Main.fs
@@ -10,19 +10,19 @@ module Main =
     let main args = 
         UIApplication.Main(args, null, "AppDelegate")
 
-//        let idAssigner = new System.Random ()
+        let idAssigner = new System.Random ()
         Listener.startListening ()
-//        let server = new Server ()
+        let server = new Server ()
 
-//        let generator i = async {
-//            let id = idAssigner.Next (1,1000000)
-//            let client = new Client (server, id)
-//            ignore <| client.Run 1000 10
-//            }
-//
-//        let parallelClients = 
-//            List.init 1 generator
-//            |> Async.Parallel
-//            |> Async.RunSynchronously
+        let generator i = async {
+            let id = idAssigner.Next (1,1000000)
+            let client = new Client (server, id)
+            ignore <| client.Run 1000 10
+            }
+
+        let parallelClients = 
+            List.init 1 generator
+            |> Async.Parallel
+            |> Async.RunSynchronously
 
         0
