87dc47bd8c17aa037e2982f66f26ac0f89cf50a8
Chaaru Dingankar <chaaru@beyondgames.co>
2015-09-02 12:07:03 -0700

    Finished refactor, added Xamarin.Amplitude.Ios on new machine.

--- SlotMachine/AppDelegate.fs
+++ SlotMachine/AppDelegate.fs
@@ -1,6 +1,6 @@
 ﻿namespace SlotMachine
 
-//open AmplitudeSdk
+open AmplitudeSdk
 
 open System
 
@@ -20,30 +20,33 @@ type AppDelegate () =
     // This method is invoked when the application is ready to run.
     override this.FinishedLaunching (app, options) =
 
-//        let api_key = System.IO.File.ReadAllText "/Users/chaaru/Projects/game/Slots.iOS/app.config"
-
+//        let api_key = System.IO.File.ReadAllText "/Users/chaaru/slot-machine/SlotsApp.iOS/api_key.config"
 //        let amp = Amplitude.Instance
 //        match amp with
 //        | null -> printfn "why is Amplitude.Instance null?: %A" amp
 //        | _ -> amp.InitializeApiKey (api_key)
-//        //Amplitude.Instance.LogEvent ("iOS test event")
+//        Amplitude.Instance.LogEvent ("iOS test event")
 //        printfn "done for now"
 
-//        let idAssigner = new System.Random ()
-//        Listener.startListening ()
-//        let server = new Server ()
-//
-//        let generator i = async {
-//            let id = idAssigner.Next (1,1000000)
-//            let client = new Client (server, id)
-//            ignore <| client.Run 1000 10
-//            }
-//
-//        let parallelClients = 
-//            List.init 3 generator
-//            |> Async.Parallel
-//            |> Async.RunSynchronously
+//    ================================================
+
+        let idAssigner = new System.Random ()
+        Listener.startListening ()
+        let server = new Server ()
+
+        let generator i = async {
+            let id = idAssigner.Next (1,1000000)
+            let client = new Client (server, id)
+            ignore <| client.Run 1000 10
+            }
+
+        let parallelClients = 
+            List.init 3 generator
+            |> Async.Parallel
+            |> Async.RunSynchronously
 
         Async.RunSynchronously <| Async.Sleep 5000
+
+//    ================================================
 
         true
--- Slots/KinesisProvider.fs
+++ Slots/KinesisProvider.fs
@@ -1,45 +1,45 @@
 ﻿module KinesisProvider
-//
-//open FSharp.Configuration
-//
-//open Amazon.Kinesis.Model
-//open Amazon.Kinesis
-//open Amazon
-//
-//open Account
-//open Metric
-//open Nessos.FsPickler
-//open FSharp.Configuration
-//
-//type Settings = AppSettings<"app.config">
-//
-//type KinesisProvider () = 
-//
-//    member self.testRun () = 
-//        let r = new System.Random ()
-//        let client = self.setup ()
-//        while true do
-//            let tuple = (55555, GameEnded)
-//            let pickler = FsPickler.CreateBinarySerializer ()
-//            let pickle = pickler.Pickle tuple
-////            let bytes : byte array = Array.create 20 0uy
-////            r.NextBytes bytes
-//            ignore <| self.provide pickle client
-//            Async.RunSynchronously <| Async.Sleep 20
-//
-//    member self.setup () = 
-//        let awsAccessKeyId = Settings.AwsAccessKeyId
-//        let awsSecretAccessKey = Settings.AwsSecretAccessKey
-//        let region = RegionEndpoint.USEast1
-//        new AmazonKinesisClient (awsAccessKeyId, awsSecretAccessKey, region)
-//
-//    member self.provide (data : byte[]) (kinesisClient : AmazonKinesisClient) = 
-//        let putRecord = new PutRecordRequest ()
-//        putRecord.StreamName <- "SlotMachineProducerStream"
-//        putRecord.PartitionKey <- "blahblahblah"
-//        printfn "sent data" // %A" data
-//        putRecord.Data <- (new System.IO.MemoryStream (data))
-//        try
-//            kinesisClient.PutRecord(putRecord)
-//        with
-//            | _ -> failwith "Error sending record to Amazon Kinesis"
+
+open FSharp.Configuration
+
+open Amazon.Kinesis.Model
+open Amazon.Kinesis
+open Amazon
+
+open Account
+open Metric
+open Nessos.FsPickler
+open FSharp.Configuration
+
+type Settings = AppSettings<"app.config">
+
+type KinesisProvider () = 
+
+    member self.testRun () = 
+        let r = new System.Random ()
+        let client = self.setup ()
+        while true do
+            let tuple = (55555, GameEnded)
+            let pickler = FsPickler.CreateBinarySerializer ()
+            let pickle = pickler.Pickle tuple
+//            let bytes : byte array = Array.create 20 0uy
+//            r.NextBytes bytes
+            ignore <| self.provide pickle client
+            Async.RunSynchronously <| Async.Sleep 20
+
+    member self.setup () = 
+        let awsAccessKeyId = Settings.AwsAccessKeyId
+        let awsSecretAccessKey = Settings.AwsSecretAccessKey
+        let region = RegionEndpoint.USEast1
+        new AmazonKinesisClient (awsAccessKeyId, awsSecretAccessKey, region)
+
+    member self.provide (data : byte[]) (kinesisClient : AmazonKinesisClient) = 
+        let putRecord = new PutRecordRequest ()
+        putRecord.StreamName <- "SlotMachineProducerStream"
+        putRecord.PartitionKey <- "blahblahblah"
+        printfn "sent data" // %A" data
+        putRecord.Data <- (new System.IO.MemoryStream (data))
+        try
+            kinesisClient.PutRecord(putRecord)
+        with
+            | _ -> failwith "Error sending record to Amazon Kinesis"
