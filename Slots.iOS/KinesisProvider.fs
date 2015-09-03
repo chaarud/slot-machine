9605806ad6b2f6d82f046dac73792daaf237e8ea
Chaaru Dingankar <chaaru@beyondgames.co>
2015-09-02 17:44:29 -0700

    intellisense and type declarations broken.

--- Slots.iOS/KinesisProvider.fs
+++ Slots.iOS/KinesisProvider.fs
@@ -1,31 +1,28 @@
 ﻿module KinesisProvider
 
-//open Amazon.Kinesis.Model
-//open Amazon.Kinesis
-//open Amazon
-//
-//open Account
-//open Metric
-//open Nessos.FsPickler
-//open FSharp.Configuration
-//
-//type Settings = AppSettings<"app.config">
-//
-//type KinesisProvider () = 
-//
-//    member self.setup () = 
-//        let awsAccessKeyId = System.IO.File.ReadAllText "/Users/chaaru/slot-machine/Slots.iOS/access_key_id.config"
-//        let awsSecretAccessKey = System.IO.File.ReadAllText "/Users/chaaru/slot-machine/Slots.iOS/secret_access_key.config"
-//        let region = RegionEndpoint.USEast1
-//        new AmazonKinesisClient (awsAccessKeyId, awsSecretAccessKey, region)
-//
-//    member self.provide (data : byte[]) (kinesisClient : AmazonKinesisClient) = 
-//        let putRecord = new PutRecordRequest ()
-//        putRecord.StreamName <- "SlotMachineProducerStream"
-//        putRecord.PartitionKey <- "blahblahblah"
-//        printfn "sent data" // %A" data
-//        putRecord.Data <- (new System.IO.MemoryStream (data))
-//        try
-//            kinesisClient.PutRecord(putRecord)
-//        with
-//            | _ -> failwith "Error sending record to Amazon Kinesis"
+open Amazon.Kinesis.Model
+open Amazon.Kinesis
+open Amazon
+
+open Account
+open Metric
+open Nessos.FsPickler
+
+type KinesisProvider () = 
+
+    member self.setup () = 
+        let awsAccessKeyId = System.IO.File.ReadAllText "/Users/chaaru/slot-machine/Slots.iOS/access_key_id.config"
+        let awsSecretAccessKey = System.IO.File.ReadAllText "/Users/chaaru/slot-machine/Slots.iOS/secret_access_key.config"
+        let region = RegionEndpoint.USEast1
+        new AmazonKinesisClient (awsAccessKeyId, awsSecretAccessKey, region)
+
+    member self.provide (data : byte[]) (kinesisClient : AmazonKinesisClient) = 
+        let putRecord = new PutRecordRequest ()
+        putRecord.StreamName <- "SlotMachineProducerStream"
+        putRecord.PartitionKey <- "blahblahblah"
+        printfn "sent data" // %A" data
+        putRecord.Data <- (new System.IO.MemoryStream (data))
+        try
+            kinesisClient.PutRecord(putRecord)
+        with
+            | _ -> failwith "Error sending record to Amazon Kinesis"
