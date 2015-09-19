89824add9df157e246e3381dc7fb3bd5d0e684c4
Neil <neil@beyondgames.co>
2015-09-18 21:26:26 -0700

    Starting Listener when server is run.

--- Server/Program.fs
+++ Server/Program.fs
@@ -3,6 +3,10 @@
 [<EntryPoint>]
 let main argv = 
 
+    async {Listener.startListening ()} |> Async.Start
+
+    System.Threading.Thread.Sleep 1000
+
     let server = new Server ()
     async {do server.Run ()} |> Async.Start
 
