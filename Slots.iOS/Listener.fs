49cfdef6cc73196d699f5f7ff854e7a650d54521
Darshdeep Hora <hora@beyondgames.co>
2015-09-18 17:30:36 -0700

    removed debug code

--- Slots.iOS/Listener.fs
+++ Slots.iOS/Listener.fs
@@ -20,14 +20,9 @@ type KinesisService () =
         publisher.createPutRecordRequest (StreamName "Slots") (PartitionKey "partition0") e.RawData
         |> publisher.publish
         |> ignore
-    
-type SendJSON () = 
-    inherit WebSocketBehavior ()
-    override self.OnMessage (e: MessageEventArgs) = 
-//        let deserialized = 
-        printfn "receiving data : %A" e.Data//todo: jsonparsing function by looking at anadashboard
+
 
 let startListening () = 
     let wsServer = new WebSocketServer("ws://localhost:55555")
-    wsServer.AddWebSocketService<SendJSON>("/KinesisService")
+    wsServer.AddWebSocketService<KinesisService>("/KinesisService")
     wsServer.Start ()
